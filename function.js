17b34c3f4b33b23df713b62d8162c59e58c24a99
stabordan <99156112+stabordan@users.noreply.github.com>
2024-05-19 10:08:19 +0800

    Add files via upload

--- function.js
+++ function.js
@@ -1,31 +1,44 @@
 // let section = document.querySelector(".skills");
 // let spans = document.querySelectorAll(".bar");
-// var section = document.getElementById('skills');
+
+
+// window.onscroll = function(){
+// 	const section = document.getElementById("skills");
+// 	const spans = section.querySelectorAll(".bar");
+// 	if (window.scrollY >= section.offsetTop) {
+// 		console.log("Reached Section Skills");
+// 		spans.forEach((span) => {
+// 			span.style.width = span.dataset.width;
+// 		});
+
+// 	}
+// };
 
 
 window.onscroll = function(){
-	const section = document.getElementById('skills');
+	const section = document.getElementById("skills");
 	const spans = section.querySelectorAll(".bar");
+
+	// const html = document.querySelector(".html");
+	// const css = section.querySelector(".css");
+	// const photoshop = section.querySelector(".photoshop");
+	// const illustrator = section.querySelector(".illustrator");
+	// const javascript = section.querySelector(".javascript");
+	
 	if (window.scrollY >= section.offsetTop) {
 		console.log("Reached Section Skills");
 		spans.forEach((span) => {
-			span.style.width = span.dataset.width;
+			span.style.animation = "spans 5s forwards";
 		});
+
+
+		// html.style.animation = "html 5s forwards";
+		// css.style.animation = "css 5s forwards";
+		// photoshop.style.animation = "photoshop 5s forwards";
+		// illustrator.style.animation = "illustrator 5s forwards";
+		// javascript.style.animation = "javascript 5s forwards";
+
 	}
 };
-
-
-
-// var offsetTop = $('#skills').offset().top;
-// $(window).scroll(function() {
-// 	var height = $(window).height();
-// 	if ($(window).scrollTop()+height > offsetTop) {
-// 		jQuery('.bar').each(function(){
-// 			jQuery(this).find('.bar').animate({
-// 				width:jQuery(this).attr('data-percent')
-// 			},2000);
-// 		});
-// 	}
-// });
 
 
